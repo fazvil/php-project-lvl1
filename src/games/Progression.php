1cdd0774affc130f41132148bbca762a7ad3ec34
Vildan Fazlyev <fazvil@mail.ru>
2020-03-11 21:41:07 +0300

    rename variable

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -14,19 +14,19 @@ function gameProgression()
         $startNum = rand(1, 5);  // Стартовое число
         $step = rand(1, 5);      // Шаг
         $empty = rand(1, $valueListLength);    // Позиция пустого значения
-        $str = '';               // Получаемый вывод
-        $value = $startNum;      // Добавляемое в $str значение
+        $question = '';               // Получаемый вывод
+        $value = $startNum;      // Добавляемое в $question значение
         for ($j = 1; $j <= $valueListLength; $j++) {
             if ($j === $empty) {
-                $str .= '.. ';
+                $question .= '.. ';
                 $value += $step;
                 $answer = $value;
             } else {
                 $value += $step;
-                $str .= (string) $value . ' ';
+                $question .= (string) $value . ' ';
             }
         }
-        $data[] = [$str,(string) $answer];
+        $data[] = [$question,(string) $answer];
     }
     run(RULES_GAME, $data);
 }
