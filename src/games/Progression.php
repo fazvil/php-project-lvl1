269e52a0316f3f9183658a9703d15b1c9332a8d4
Vildan Fazlyev <fazvil@mail.ru>
2020-03-11 22:12:55 +0300

    rewrite logik cod

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -8,21 +8,19 @@ use function BrainGames\Cli\run;
 const RULES_GAME = 'What number is missing in the progression?';
 function gameProgression()
 {
-    $valueListLength = 10;
+    $listLength = 10;
     $data = [];
     for ($i = 0; $i < AMOUND_ROUNDS; $i++) {
         $startNum = rand(1, 5);  // Стартовое число
         $step = rand(1, 5);      // Шаг
-        $empty = rand(1, $valueListLength);    // Позиция пустого значения
-        $question = '';               // Получаемый вывод
-        $value = $startNum;      // Добавляемое в $question значение
-        for ($j = 1; $j <= $valueListLength; $j++) {
+        $empty = rand(0, $listLength);    // Позиция пустого значения
+        $question = '';          // Получаемый вывод
+        for ($j = 0; $j < $listLength; $j++) {
+            $value = $startNum + $step * $j;
             if ($j === $empty) {
                 $question .= '.. ';
-                $value += $step;
                 $answer = $value;
             } else {
-                $value += $step;
                 $question .= (string) $value . ' ';
             }
         }
