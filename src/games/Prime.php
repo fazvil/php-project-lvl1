6357827465b0179556dd369640457aab51bd8710
Vildan Fazlyev <fazvil@mail.ru>
2020-03-12 15:11:19 +0300

    rewrite cod

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -6,22 +6,27 @@ use const BrainGames\Cli\AMOUND_ROUNDS;
 use function BrainGames\Cli\run;
 
 const RULES_GAME = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime($num)
+{
+    for ($divider = 2; $divider < $num; $divider++) {
+        if (
+            $num === 1
+            || $num % 2 === 0
+            || $num % $divider === 0
+        ) {
+            return 'no';
+        }
+    }
+    return 'yes';
+}
+
 function gamePrime()
 {
     $data = [];
     for ($i = 0; $i < AMOUND_ROUNDS; $i++) {
-        $question = rand(2, 100);
-        if ($question == 2 || $question == 3) {
-            $answer = 'yes';
-        }
-        for ($divider = 2; $divider <= intdiv($question, 2); $divider++) {
-            if ($question % $divider == 0) {
-                $answer = 'no';
-                break;
-            } else {
-                $answer = 'yes';
-            }
-        }
+        $question = rand(1, 100);
+        $answer = isPrime($question);
         $data[] = [$question, $answer];
     }
     run(RULES_GAME, $data);
