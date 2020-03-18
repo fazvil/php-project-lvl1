2cd44d73b17565c29d637f5ae65423005b9b9a99
Vildan Fazlyev <fazvil@mail.ru>
2020-03-18 20:20:31 +0300

    rewrite

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -8,24 +8,32 @@ use const BrainGames\Cli\ROUNDS_COUNT;
 
 const GAME_DESCRIPTION = 'What number is missing in the progression?';
 
+function getProgressionList($progressionLength, $startNum, $step)
+{
+    $result = [];
+    for ($j = 0; $j < $progressionLength; $j++) {
+        $result[] = $startNum + $step * $j;
+    }
+    return $result;
+}
+
 function gameProgression()
 {
     $progressionLength = 10;
+    $hiddenElement = '..';
     $data = [];
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $startNum = rand(1, 5);
         $step = rand(1, 5);
         $hiddenElementPosition = rand(0, $progressionLength);
-        $hiddenElement = '..';
         $question = '';
-        for ($j = 0; $j < $progressionLength; $j++) {
-            $addedElement = $startNum + $step * $j;
-            if ($j === $hiddenElementPosition) {
-                $answer = $addedElement;
-                $question = trim("{$question} {$hiddenElement}");
-            } else {
-                $question = trim("{$question} {$addedElement}");
+        $progressionList = getProgressionList($progressionLength, $startNum, $step);
+        foreach ($progressionList as $key => $element) {
+            if ($key === $hiddenElementPosition) {
+                $answer = $element;
+                $element = $hiddenElement;
             }
+            $question = trim("{$question} {$element}");
         }
         $data[] = [$question, (string) $answer];
     }
